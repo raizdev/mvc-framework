dd03011a1d8d8c3c342c2f5e981c058fa93950c8
Janis Schubert <j.schubert@netz98.de>
2020-07-23 18:52:44 +0200

    [Task] Added doc block for helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,9 +1,13 @@
 <?php
 
-/**
- * Takes message and placeholder to translate them to global locale.
- */
 if (!function_exists('__')) {
+    /**
+     * Takes message and placeholder to translate them to global locale.
+     *
+     * @param string $message
+     * @param array $placeholder
+     * @return mixed
+     */
     function __(string $message, array $placeholder = []) {
         $container = \Ares\Framework\Proxy\App::getContainer();
         $localeService = $container->get(\Ares\Framework\Service\LocaleService::class);
@@ -11,19 +15,23 @@ if (!function_exists('__')) {
     }
 }
 
-/**
- * Returns directory path of app.
- */
 if (!function_exists('app_dir')) {
+    /**
+     * Returns directory path of app.
+     *
+     * @return string
+     */
     function app_dir() {
         return __DIR__;
     }
 }
 
-/**
- * Returns directory path of root.
- */
 if (!function_exists('base_dir')) {
+    /**
+     * Returns directory path of root.
+     *
+     * @return string
+     */
     function base_dir() {
         return __DIR__ . '/../';
     }
