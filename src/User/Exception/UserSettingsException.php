5a751973f068e1ac65563b95d270fd9b76a6ea3a
s1njar <s1njar.mail@gmail.com>
2020-09-06 01:22:23 +0200

    [TASK] Added services to change several user settings

---
+++ src/User/Exception/UserSettingsException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Ares (https://ares.to)
+ *
+ * @license https://gitlab.com/arescms/ares-backend/LICENSE (MIT License)
+ */
+
+namespace Ares\User\Exception;
+
+use Ares\Framework\Exception\BaseException;
+
+/**
+ * Class UserSettingsException
+ *
+ * @package Ares\User\Exception
+ */
+class UserSettingsException extends BaseException
+{
+}
---
+++ src/User/Service/Settings/ChangeGeneralSettingsService.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Ares (https://ares.to)
+ *
+ * @license https://gitlab.com/arescms/ares-backend/LICENSE (MIT License)
+ */
+
+namespace Ares\User\Service\Settings;
+
+use Ares\Framework\Interfaces\CustomResponseInterface;
+use Ares\User\Entity\User;
+use Ares\User\Entity\UserSetting;
+use Ares\User\Exception\UserSettingsException;
+use Ares\User\Repository\UserSettingRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Phpfastcache\Exceptions\PhpfastcacheSimpleCacheException;
+use Psr\Cache\InvalidArgumentException;
+
+/**
+ * Class ChangeGeneralSettingsService
+ *
+ * @package Ares\User\Service\Settings
+ */
+class ChangeGeneralSettingsService
+{
+    /**
+     * @var UserSettingRepository
+     */
+    private UserSettingRepository $userSettingRepository;
+
+    /**
+     * ChangeGeneralSettingsService constructor.
+     *
+     * @param UserSettingRepository $userSettingRepository
+     */
+    public function __construct(
+        UserSettingRepository $userSettingRepository
+    ) {
+        $this->userSettingRepository = $userSettingRepository;
+    }
+
+    /**
+     * Changes user general settings by given user.
+     *
+     * @param User $user
+     * @param array $data
+     * @return CustomResponseInterface
+     * @throws InvalidArgumentException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     * @throws PhpfastcacheSimpleCacheException
+     * @throws UserSettingsException
+     */
+    public function execute(User $user, array $data): CustomResponseInterface
+    {
+        $userSetting = $this->userSettingRepository->getBy(['user' => $user]);
+
+        if (!$userSetting) {
+            throw new UserSettingsException(__('Settings for given user does not exist.'));
+        }
+
+        $userSetting = $this->userSettingRepository->update($this->getUpdatedUserSettings($userSetting, $data));
+
+        return response()->setData($userSetting);
+    }
+
+    /**
+     * Returns updated user settings model.
+     *
+     * @param UserSetting $userSetting
+     * @param array $data
+     * @return UserSetting
+     */
+    private function getUpdatedUserSettings(UserSetting $userSetting, array $data): UserSetting
+    {
+        return $userSetting
+            ->setBlockFollowing($data['block_following'])
+            ->setBlockFriendrequests($data['block_friendrequests'])
+            ->setBlockRoominvites($data['block_roominvites'])
+            ->setBlockCameraFollow($data['block_camera_follow'])
+            ->setBlockAlerts($data['block_alerts'])
+            ->setIgnoreBots($data['ignore_bots'])
+            ->setIgnorePets($data['ignore_pets']);
+    }
+}
