c6bad41bd57d1db596fbee3d60ed3b42b7f30000
Dome <dominik-forschner@web.de>
2020-09-13 00:31:51 +0200

    [TASK] added ip check to throttle

--- src/Framework/Middleware/ThrottleMiddleware.php
+++ src/Framework/Middleware/ThrottleMiddleware.php
@@ -145,6 +145,22 @@ class ThrottleMiddleware implements MiddlewareInterface
      */
     private function getIdentifier()
     {
-        return $_SERVER['REMOTE_ADDR'] . $_SERVER['REQUEST_URI'];
+        return $this->determineIp() . $_SERVER['REQUEST_URI'];
+    }
+
+    /**
+     * @return string|null
+     */
+    private function determineIp(): ?string
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ip;
     }
 }
