72e30cd1c624fe891f92d41ad30aae2ac0d8b32c
Dome <dominik-forschner@web.de>
2020-10-13 23:52:22 +0200

    [TASK] Changed value of ratelimiting requests

--- src/Framework/Provider/ThrottleServiceProvider.php
+++ src/Framework/Provider/ThrottleServiceProvider.php
@@ -36,7 +36,7 @@ class ThrottleServiceProvider extends AbstractServiceProvider
 
             $throttleMiddleware = new ThrottleMiddleware($predis);
             $throttleMiddleware
-                ->setRateLimit(10, 5)
+                ->setRateLimit(8, 1)
                 ->setStorageKey('ARES_API_THROTTLE:%s');
 
             return $throttleMiddleware;
