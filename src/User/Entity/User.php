ba0cbaf641b36133c2f2979eeccb70682a0508a3
Dome <dominik-forschner@web.de>
2020-10-19 00:05:16 +0200

    [WIP] Added more hidden fields

--- src/User/Entity/User.php
+++ src/User/Entity/User.php
@@ -25,7 +25,10 @@ class User extends DataObject implements UserInterface
 
     /** @var array */
     public const HIDDEN = [
-        UserInterface::COLUMN_PASSWORD
+        UserInterface::COLUMN_PASSWORD,
+        UserInterface::COLUMN_AUTH_TICKET,
+        UserInterface::COLUMN_IP_CURRENT,
+        UserInterface::COLUMN_IP_REGISTER
     ];
 
     /**
